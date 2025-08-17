d584909a90d3ac26e07bc85230afd491ff872b29
paarthtiwari <parth.tiwari-1@student.uts.edu.au>
2025-08-17 11:29:30 +1000

    add split_sets_random() for random train/val/test splitting

--- src/my_krml_25506751/data/sets.py
+++ src/my_krml_25506751/data/sets.py
@@ -100,4 +100,43 @@ def split_sets_by_time(df: pd.DataFrame, target_col: str, test_ratio: float = 0.
     X_test,  y_test  = subset_x_y(target=y, features=df_copy, start_index=n - cutoff,   end_index=n)
 
     return X_train, y_train, X_val, y_val, X_test, y_test
+    
+def split_sets_random(features, target, test_ratio=0.2):
+    """Split sets randomly
+
+    Parameters
+    ----------
+    features : pd.DataFrame
+        Input dataframe
+    target : pd.Series
+        Target column
+    test_ratio : float
+        Ratio used for the validation and testing sets (default: 0.2)
+
+    Returns
+    -------
+    Numpy Array
+        Features for the training set
+    Numpy Array
+        Target for the training set
+    Numpy Array
+        Features for the validation set
+    Numpy Array
+        Target for the validation set
+    Numpy Array
+        Features for the testing set
+    Numpy Array
+        Target for the testing set
+    """
+    from sklearn.model_selection import train_test_split
+
+    val_ratio = test_ratio / (1 - test_ratio)
+    X_data, X_test, y_data, y_test = train_test_split(
+        features, target, test_size=test_ratio, random_state=8
+    )
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_data, y_data, test_size=val_ratio, random_state=8
+    )
+
+    return X_train, y_train, X_val, y_val, X_test, y_test
 
